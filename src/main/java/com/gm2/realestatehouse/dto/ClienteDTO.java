5fd2eaec1722f44b29eb116bbd6729bc5fa43c0e
Victor Lelis <victorlelis2011@gmail.com>
2024-02-01 21:21:00 -0400

    Atualizando dto

--- src/main/java/com/gm2/realestatehouse/dto/ClienteDTO.java
+++ src/main/java/com/gm2/realestatehouse/dto/ClienteDTO.java
@@ -2,15 +2,23 @@ package com.gm2.realestatehouse.dto;
 
 import com.gm2.realestatehouse.entity.enums.EstadoCivil;
 import com.gm2.realestatehouse.entity.enums.Sexo;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
 public record ClienteDTO(
+        @NotBlank(message = "Campo nome Inválido: vazio")
         String nome,
+        @NotBlank(message = "Campo cpf Inválido: vazio")
         String cpf,
+        @NotBlank(message = "Campo telefone Inválido: vazio")
         String telefone,
+        @NotBlank(message = "Campo email Inválido: vazio")
         String email,
+        @NotNull(message = "Campo sexo Inválido: vazio")
         Sexo sexo,
+        @NotNull(message = "Campo estado Inválido: vazio")
         EstadoCivil estadoCivil) {
 
 }
