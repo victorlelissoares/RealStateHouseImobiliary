2a16c743f48c2ee4fa3a6e2c398c64e44d7b378d
Victor Lelis <victorlelis2011@gmail.com>
2024-02-01 21:21:25 -0400

    Atualizando controller

--- src/main/java/com/gm2/realestatehouse/controller/ClienteController.java
+++ src/main/java/com/gm2/realestatehouse/controller/ClienteController.java
@@ -1,21 +1,22 @@
 package com.gm2.realestatehouse.controller;
 
 import com.gm2.realestatehouse.dto.ClienteDTO;
-import com.gm2.realestatehouse.helper.mapper.ClienteMapper;
 import com.gm2.realestatehouse.service.ClienteService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
+@Validated
 @RestController
 @AllArgsConstructor
 @RequestMapping("/clientes")
@@ -28,22 +29,19 @@ public class ClienteController {
     }
 
     @PostMapping
-    public ResponseEntity<Long> create(@RequestBody ClienteDTO clienteDTO){
+    public ResponseEntity<Long> create(@Valid @RequestBody ClienteDTO clienteDTO){
         return ResponseEntity.ok(clienteService.create(clienteDTO));
     }
 
     @DeleteMapping("/{idCliente}")
-    public ResponseEntity<Void> delete (@PathVariable Long idCliente){
+    public ResponseEntity<Void> delete (@Valid @PathVariable Long idCliente){
         clienteService.delete(idCliente);
         return ResponseEntity.noContent().build();
     }
 
     @PutMapping
-    public ResponseEntity<Void> update (@RequestBody ClienteDTO clienteDTO){
+    public ResponseEntity<Void> update (@Valid @RequestBody ClienteDTO clienteDTO){
         clienteService.update(clienteDTO);
         return ResponseEntity.ok().build();
     }
-
-
-
 }
